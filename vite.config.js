92efa3b571c2af6011153ce3eec205ff8a366bb9
Anubhavsingh9905 <anubhavsingh1665@gmail.com>
2025-10-05 16:42:28 +0530

    Add new features

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    allowedHosts: "find-me-1.onrender.com",
     host: true,
     port: 5173,
   }
